0a0605148f5cb1e2934358e285f71ef7feb9e5f5
Marcel Breyer <breyer.marcel@web.de>
2021-04-13 21:59:01 +0200

    added additional examples

--- examples.cpp
+++ examples.cpp
@@ -1,5 +1,5 @@
+#include <cstring>
 #include <iostream>
-#include <string>
 #include <string_view>
 
 #include "cstring_view.hpp"
@@ -37,7 +37,46 @@ int main() {
 
   std::cout << std::endl << std::boolalpha;
   std::cout << (csv.size() == csv.length()) << std::endl;
-  std::cout << csv.empty() << std::endl;
+  std::cout << csv.empty() << std::endl << std::endl;
+
+  std::cout << (csv[1] == 'e') << std::endl;
+  std::cout << (csv.at(2) == 'l') << std::endl;
+  std::cout << (csv.front() == 'H') << std::endl;
+  std::cout << (csv.back() == '!') << std::endl;
+  std::cout << (std::strcmp(csv.c_str(), "Hello, World!") == 0) << std::endl;
+  std::cout << static_cast<std::string_view>(csv) << std::endl;
+
+  auto csv_copy = csv;
+  csv_copy.remove_prefix(7);
+  std::cout << csv_copy << std::endl;
+  auto csv_copy2 = csv;
+  std::cout << csv_copy2.substr(7) << std::endl;
+
+  std::cout << csv.compare("12345") << std::endl;
+  std::cout << csv.compare("Hello") << std::endl;
+  std::cout << csv.compare(csv) << std::endl;
+
+  std::cout << csv.starts_with("Hello") << std::endl;
+  std::cout << csv.ends_with("World!") << std::endl;
+  std::cout << csv.contains("123") << std::endl;
+
+  std::cout << csv.find("World!") << std::endl;
+  std::cout << csv.find('o') << std::endl;
+  std::cout << csv.rfind('o') << std::endl;
+
+  std::cout << csv.find_first_of('o') << std::endl;
+  std::cout << csv.find_last_of('o') << std::endl;
+  std::cout << csv.find_first_not_of("Hl!") << std::endl;
+  std::cout << csv.find_last_not_of("Hl!") << std::endl;
+
+  auto csv2 = "Hello, World!"_csv;
+  auto csv3 = "12345"_csv;
+  std::cout << (csv == csv2) << std::endl;
+  std::cout << (csv == csv3) << std::endl;
+  std::cout << (csv != csv2) << std::endl;
+  std::cout << (csv != csv3) << std::endl;
+
+  std::cout << csv << std::endl;
 
   return 0;
 }
