02386460505f8b252ce44401bf18b80ac4bb6310
Marcel Breyer <breyer.marcel@web.de>
2024-09-05 21:42:01 +0200

    Add test dummies.

---
+++ tests/catch/catch_main.cpp
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2021-06-06 - Marcel Breyer - All Rights Reserved
+ * Licensed under the MIT License. See LICENSE.md file in the project root for full license information.
+ *
+ * Main file for Catch2 test framework.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
